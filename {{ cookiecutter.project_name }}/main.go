bb7f7121901347b95680b7945866ba9bb0faa9bf
mjehanno <mathob.jehanno@hotmail.fr>
2022-07-01 17:13:00 +0200

    feat(app): adds systray menu to the app

--- {{ cookiecutter.project_name }}/main.go
+++ {{ cookiecutter.project_name }}/main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/driver/desktop"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("{{ cookiecutter.project_name }}")
+
+	if desk, ok := a.(desktop.App); ok {
+		menu := fyne.NewMenu("{{ cookiecutter.project_name }}", nil)
+		desk.SetSystemTrayMenu(m)
+	}
+
 	w.Resize(fyne.NewSize(200, 100))
 	w.ShowAndRun()
 }
